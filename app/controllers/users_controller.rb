aba66102708efedbc5385aeafff9af025a31c9f6
隼人 <takebnn26@gmail.com>
2016-08-17 11:28:49 +0900

    マイページボタン実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
     @tweets = Tweet.where(params[:id]).order("created_at DESC").page(params[:page]).per(5)
   end
 end
