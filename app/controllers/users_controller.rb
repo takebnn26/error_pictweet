c84fe6daddd6872be05390c2fd8230b7e7febb8d
隼人 <takebnn26@gmail.com>
2016-08-17 11:23:50 +0900

    showビュー実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.id
+    @nickname = current_user.nickname
     @tweets = Tweet.where(params[:id]).order("created_at DESC").page(params[:page]).per(5)
   end
 end
