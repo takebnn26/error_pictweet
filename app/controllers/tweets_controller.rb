e630d5ee674d3b93f7e310d7a995aea77b987b4c
隼人 <takebnn26@gmail.com>
2016-08-17 12:20:55 +0900

    destroy機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,31 @@
 class TweetsController < ApplicationController
-  before_action :move_to_index, except: :index
-  def index
-    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
-  end
 
-  def new
-  end
+    before_action :move_to_index, except: :index
 
-  def create
-    Tweet.create(text: tweet_params[:text], image: tweet_params[:image], user_id: current_user.id)
-  end
+    def index
+      @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    end
 
-  private
-  def tweet_params
-    params.permit(:text,:image)
-  end
+    def new
+    end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    def create
+      Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    end
+
+    def destroy
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+        tweet.destroy
+      end
+    end
+
+    private
+    def tweet_params
+      params.permit(:image, :text)
+    end
+
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
   end
-end
