3d7dd2902a84a5d0a59cd405758872ea93186750
隼人 <takebnn26@gmail.com>
2016-08-18 11:27:26 +0900

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root   "tweets#index"
-  get    'tweets'     => 'tweets#index'
-  get    'tweets/new' => 'tweets#new'
-  post   'tweets'     => 'tweets#create'
-  get    'users/:id'  => 'users#show'
-  delete 'tweets/:id' => 'tweets#destroy'
-  get 'tweets/:id/edit' => 'tweets#edit'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users,  only: [:show]
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817025102) do
+ActiveRecord::Schema.define(version: 20160818005512) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text",       limit: 255
+    t.integer  "user_id",    limit: 4
+    t.integer  "tweet_id",   limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text     "text",       limit: 65535
