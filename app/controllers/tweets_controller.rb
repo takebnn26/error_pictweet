21044c7a507a5341c2e8ac798e3bffec451a6ed3
hayato takemoto <takebnn26@gmail.com>
2016-11-17 14:42:45 +0900

    tweets/edit.html.erbのform_tag の = を外す tweets/show.html.erbの削除ボタンのメソッドをgetにした 部分テンプレートファイルの_をなくした tweets_controller/indexアクションの@tweetsを@tweetに変更 users_controller/showアクションの@tweetsのorderメソッド引数をcreated_atとDESCを分けた

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
     before_action :move_to_index, except: :index
 
     def index
-      @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+      @tweet = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
     end
 
     def new
